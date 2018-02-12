778aa70471764087da05d8145b4cb42b53795b47
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-02-12 20:11:06 +1300

    Update javascript.

--- public/_static/ie-corner.js
+++
@@ -1,9 +0,0 @@
-
-// For IE... -_-
-$('.tl a').corner("tl 40px");
-$('.bl a').corner("bl 40px");
-$('.tr a').corner("tr 40px");
-
-$('#main-header').corner("10px");
-
-$('.header .menu .link').corner("tl tr 10px")
